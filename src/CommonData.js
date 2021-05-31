83bb29dedf9b6b9d6bb37db9c3966d83b9f4a485
klirola <khose.lirola@dataart.com>
2021-05-31 11:34:13 +0300

    new advertisement page cleaned code
    added additional services and uploading images
    
    added categories definition
    
    created ad form and added types filling

--- src/CommonData.js
+++ src/CommonData.js
@@ -2,3 +2,5 @@ export const SERVER_URL = "http://localhost:8080";
 export const API_ADVERTISEMENT = SERVER_URL + "/api/advertisement";
 export const API_ADDITIONAL_SERVICES = SERVER_URL + "/api/additionalServices";
 export const API_IMAGE = SERVER_URL + "/api/image";
+export const TYPES = SERVER_URL + "/api/types";
+export const CATEGORIES = SERVER_URL + "/api/categories";
