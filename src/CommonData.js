91a4b060cace738ae5b1b53d44a76183227dcac1
klirola <khose.lirola@dataart.com>
2021-06-29 17:49:09 +0300

    configured settings for docker-compose (remote access)

--- src/CommonData.js
+++ src/CommonData.js
@@ -1,15 +1,15 @@
-export const SERVER_URL = "http://cerebro-back:8080";
+// export const  = "http://cerebro-back:8080";
 
-export const API_ADDITIONAL_SERVICES = SERVER_URL + "/api/additionalServices";
-export const API_ADVERTISEMENT = SERVER_URL + "/api/advertisement";
-export const API_CATEGORIES = SERVER_URL + "/api/categories";
-export const API_CURRENT_USER = SERVER_URL + "/api/user";
-export const API_IMAGE = SERVER_URL + "/api/image";
-export const API_ORDER = SERVER_URL + "/api/order"
-export const API_REGISTRATION = SERVER_URL + "/api/registration";
-export const API_TYPES = SERVER_URL + "/api/types";
-export const API_USER = SERVER_URL + "/api/user";
-export const LOGIN = SERVER_URL + "/api/auth";
-export const LOGOUT = SERVER_URL + "/api/logout";
+export const API_ADDITIONAL_SERVICES ="/api/additionalServices";
+export const API_ADVERTISEMENT ="/api/advertisement";
+export const API_CATEGORIES ="/api/categories";
+export const API_CURRENT_USER ="/api/user";
+export const API_IMAGE ="/api/image";
+export const API_ORDER ="/api/order"
+export const API_REGISTRATION ="/api/registration";
+export const API_TYPES ="/api/types";
+export const API_USER ="/api/user";
+export const LOGIN ="/api/auth";
+export const LOGOUT ="/api/logout";
 
 
